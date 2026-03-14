73d695cd04fe29edf392cd07e1c63ae8a6eb64f2
ben kagarlizky <53075379+KagBen@users.noreply.github.com>
2026-03-14 07:42:00 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, onValue, ref, set } from 'firebase/database';
 import { motion } from 'framer-motion';
